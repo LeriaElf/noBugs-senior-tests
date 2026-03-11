0babf0243f9a4a9f877a2cb3b28881004d389048
LeriaE <115901737+LeriaElf@users.noreply.github.com>
2026-03-11 13:22:17 +0400

    changed front docker

--- playwright.config.js
+++ playwright.config.js
@@ -1,36 +1,36 @@
-import { defineConfig, devices } from "@playwright/test";
-import "dotenv/config";
+import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
 
 export default defineConfig({
-  testDir: "./ui/tests",
+  testDir: './ui/tests',
   fullyParallel: true,
   retries: 0,
   workers: undefined,
-  reporter: [["list"], ["html", { outputFolder: "playwright-report" }]],
+  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   use: {
-    headless: process.env.CI === "true",
-    baseURL: process.env.UI_BASE_URL || "http://localhost:3000",
+    headless: process.env.CI === 'true',
+    baseURL: process.env.UI_BASE_URL || 'http://localhost:3000',
     actionTimeout: 10_000,
-    trace: "retain-on-failure",
-    screenshot: "only-on-failure",
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
     viewport: { width: 1600, height: 1200 },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
   ],
 });
--- ui/tests/transferTest.spec.js
+++ ui/tests/transferTest.spec.js
@@ -1,13 +1,13 @@
-import { test, expect } from "../fixtures/baseUi";
-import { UserDashboard } from "../pages/userDashboard";
-import { TransferPage } from "../pages/transferPage.js";
-import { BankAlert } from "../utils/bankAlert.js";
-import { URLS } from "../utils/urls.js";
-import { parseAlertAmount, parseAlertAccount } from "../utils/patterns.js";
-import { generateNonExistentAccount } from "../utils/generateNonExistentAccount.js";
-import { setupSenderWithAccount } from "../helpers/setupSenderWithAccount.js";
+import { test, expect } from '../fixtures/baseUi';
+import { UserDashboard } from '../pages/userDashboard';
+import { TransferPage } from '../pages/transferPage.js';
+import { BankAlert } from '../utils/bankAlert.js';
+import { URLS } from '../utils/urls.js';
+import { parseAlertAmount, parseAlertAccount } from '../utils/patterns.js';
+import { generateNonExistentAccount } from '../utils/generateNonExistentAccount.js';
+import { setupSenderWithAccount } from '../helpers/setupSenderWithAccount.js';
 
-test.describe("Transfer Service Tests", () => {
+test.describe('Transfer Service Tests', () => {
   test("User should be able to transfer valid amount between user's accounts", async ({
     page,
     withUserSession,
@@ -16,9 +16,11 @@ test.describe("Transfer Service Tests", () => {
     const userDashboard = new UserDashboard(page);
 
     const { steps, firstAccount, secondAccount, userName } =
-      await test.step("Precondition: create user, authorize, create accounts", async () => {
-        const { steps, token, accounts, userName } =
-          await setupSenderWithAccount({ withUserSession }, 2);
+      await test.step('Precondition: create user, authorize, create accounts', async () => {
+        const { steps, token, accounts, userName } = await setupSenderWithAccount(
+          { withUserSession },
+          2,
+        );
         await authWithToken({
           token,
           goto: URLS.DASHBOARD,
@@ -39,41 +41,31 @@ test.describe("Transfer Service Tests", () => {
 
       await transferPage.accountForm.chooseAccount(firstAccount.accountNumber);
       await transferPage.accountForm.fillRecipientName(userName);
-      await transferPage.accountForm.fillRecipientAccount(
-        secondAccount.accountNumber,
-      );
+      await transferPage.accountForm.fillRecipientAccount(secondAccount.accountNumber);
       await transferPage.accountForm.enterAmount(firstAccount.balance);
       await transferPage.confirmCheckboxClick();
 
-      const alertMessage = await transferPage.checkAlertAndAccept(
-        BankAlert.TRANSFER_SUCCESS,
-        () => transferPage.clickTransferButton(),
+      const alertMessage = await transferPage.checkAlertAndAccept(BankAlert.TRANSFER_SUCCESS, () =>
+        transferPage.clickTransferButton(),
       );
 
       expect(parseAlertAmount(alertMessage)).toBe(firstAccount.balance);
       expect(parseAlertAccount(alertMessage)).toBe(secondAccount.accountNumber);
 
       const { accounts } = await steps.getCustomerAccaunts();
-      const senderAccount = accounts.find(
-        (acc) => acc.accountNumber === firstAccount.accountNumber,
-      );
+      const senderAccount = accounts.find(acc => acc.accountNumber === firstAccount.accountNumber);
       expect(senderAccount.balance).toBe(0);
 
       const receiverAccount = accounts.find(
-        (acc) => acc.accountNumber === secondAccount.accountNumber,
+        acc => acc.accountNumber === secondAccount.accountNumber,
       );
-      expect(receiverAccount.balance).toBe(
-        firstAccount.balance + secondAccount.balance,
-      );
+      expect(receiverAccount.balance).toBe(firstAccount.balance + secondAccount.balance);
 
       await page.reload();
-      const visibleBalance =
-        await transferPage.accountForm.getSelectedAccountBalance(
-          secondAccount.accountNumber,
-        );
-      expect(Number(visibleBalance)).toBe(
-        firstAccount.balance + secondAccount.balance,
+      const visibleBalance = await transferPage.accountForm.getSelectedAccountBalance(
+        secondAccount.accountNumber,
       );
+      expect(Number(visibleBalance)).toBe(firstAccount.balance + secondAccount.balance);
     });
   });
 
@@ -84,14 +76,8 @@ test.describe("Transfer Service Tests", () => {
   }) => {
     const userDashboard = new UserDashboard(page);
 
-    const {
-      senderSteps,
-      senderAccount,
-      recipientSteps,
-      recipientAccount,
-      recipientName,
-    } =
-      await test.step("Precondition: create users, authorize, create accounts", async () => {
+    const { senderSteps, senderAccount, recipientSteps, recipientAccount, recipientName } =
+      await test.step('Precondition: create users, authorize, create accounts', async () => {
         const {
           steps: senderSteps,
           token,
@@ -116,7 +102,7 @@ test.describe("Transfer Service Tests", () => {
         };
       });
 
-    await test.step("Transfer money between different users accounts", async () => {
+    await test.step('Transfer money between different users accounts', async () => {
       await userDashboard.clickTransferButton();
 
       const transferPage = new TransferPage(page);
@@ -124,40 +110,32 @@ test.describe("Transfer Service Tests", () => {
 
       await transferPage.accountForm.chooseAccount(senderAccount.accountNumber);
       await transferPage.accountForm.fillRecipientName(recipientName);
-      await transferPage.accountForm.fillRecipientAccount(
-        recipientAccount.accountNumber,
-      );
+      await transferPage.accountForm.fillRecipientAccount(recipientAccount.accountNumber);
       await transferPage.accountForm.enterAmount(senderAccount.balance);
       await transferPage.confirmCheckboxClick();
 
-      const alertMessage = await transferPage.checkAlertAndAccept(
-        BankAlert.TRANSFER_SUCCESS,
-        () => transferPage.clickTransferButton(),
+      const alertMessage = await transferPage.checkAlertAndAccept(BankAlert.TRANSFER_SUCCESS, () =>
+        transferPage.clickTransferButton(),
       );
 
       expect(parseAlertAmount(alertMessage)).toBe(senderAccount.balance);
-      expect(parseAlertAccount(alertMessage)).toBe(
-        recipientAccount.accountNumber,
-      );
+      expect(parseAlertAccount(alertMessage)).toBe(recipientAccount.accountNumber);
 
       const { accounts } = await senderSteps.getCustomerAccaunts();
       const senderAccountApi = accounts.find(
-        (acc) => acc.accountNumber === senderAccount.accountNumber,
+        acc => acc.accountNumber === senderAccount.accountNumber,
       );
       expect(senderAccountApi.balance).toBe(0);
 
-      const { accounts: recipientAccounts } =
-        await recipientSteps.getCustomerAccaunts();
+      const { accounts: recipientAccounts } = await recipientSteps.getCustomerAccaunts();
       const recipientAccountApi = recipientAccounts.find(
-        (acc) => acc.accountNumber === recipientAccount.accountNumber,
+        acc => acc.accountNumber === recipientAccount.accountNumber,
       );
-      expect(recipientAccountApi.balance).toBe(
-        senderAccount.balance + recipientAccount.balance,
-      );
+      expect(recipientAccountApi.balance).toBe(senderAccount.balance + recipientAccount.balance);
     });
   });
 
-  test("User should not be able to transfer to a non-existent account", async ({
+  test('User should not be able to transfer to a non-existent account', async ({
     page,
     withUserSession,
     authWithToken,
@@ -165,7 +143,7 @@ test.describe("Transfer Service Tests", () => {
     const userDashboard = new UserDashboard(page);
 
     const { steps, account, userName } =
-      await test.step("Precondition: create user, authorize, create account", async () => {
+      await test.step('Precondition: create user, authorize, create account', async () => {
         const {
           steps,
           token,
@@ -181,7 +159,7 @@ test.describe("Transfer Service Tests", () => {
         return { steps, account, userName };
       });
 
-    await test.step("Transfer money to a non-existent account", async () => {
+    await test.step('Transfer money to a non-existent account', async () => {
       await userDashboard.clickTransferButton();
       const transferPage = new TransferPage(page);
 
@@ -195,27 +173,24 @@ test.describe("Transfer Service Tests", () => {
       await transferPage.accountForm.enterAmount(account.balance);
       await transferPage.confirmCheckboxClick();
 
-      await transferPage.checkAlertAndAccept(
-        BankAlert.TRANSFER_WRONG_ACCOUNT,
-        () => transferPage.clickTransferButton(),
+      await transferPage.checkAlertAndAccept(BankAlert.TRANSFER_WRONG_ACCOUNT, () =>
+        transferPage.clickTransferButton(),
       );
 
       const { accounts } = await steps.getCustomerAccaunts();
-      const senderAccount = accounts.find(
-        (acc) => acc.accountNumber === account.accountNumber,
-      );
+      const senderAccount = accounts.find(acc => acc.accountNumber === account.accountNumber);
       expect(senderAccount.balance).toBe(account.balance);
     });
   });
 
-  test("User should not be able to transfer with blank/unchecked fields", async ({
+  test('User should not be able to transfer with blank/unchecked fields', async ({
     page,
     withUserSession,
     authWithToken,
   }) => {
     const userDashboard = new UserDashboard(page);
 
-    await test.step("Precondition: create user, authorize, create account", async () => {
+    await test.step('Precondition: create user, authorize, create account', async () => {
       const { token } = await setupSenderWithAccount({ withUserSession });
       await authWithToken({
         token,
@@ -224,20 +199,19 @@ test.describe("Transfer Service Tests", () => {
       await userDashboard.expectLoaded();
     });
 
-    await test.step("Transfer money with blank fields", async () => {
+    await test.step('Transfer money with blank fields', async () => {
       await userDashboard.clickTransferButton();
       const transferPage = new TransferPage(page);
 
       await transferPage.titleIsVisible();
 
-      await transferPage.checkAlertAndAccept(
-        BankAlert.TRANSFER_FILL_ALL_FIELDS,
-        () => transferPage.clickTransferButton(),
+      await transferPage.checkAlertAndAccept(BankAlert.TRANSFER_FILL_ALL_FIELDS, () =>
+        transferPage.clickTransferButton(),
       );
     });
   });
 
-  test("User should not be able to transfer with zero amount", async ({
+  test('User should not be able to transfer with zero amount', async ({
     page,
     withUserSession,
     authWithToken,
@@ -245,7 +219,7 @@ test.describe("Transfer Service Tests", () => {
     const userDashboard = new UserDashboard(page);
 
     const { steps, account, userName } =
-      await test.step("Precondition: create user, authorize, create account", async () => {
+      await test.step('Precondition: create user, authorize, create account', async () => {
         const {
           steps,
           token,
@@ -261,7 +235,7 @@ test.describe("Transfer Service Tests", () => {
         return { steps, account, userName };
       });
 
-    await test.step("Transfer money with zero amount", async () => {
+    await test.step('Transfer money with zero amount', async () => {
       await userDashboard.clickTransferButton();
       const transferPage = new TransferPage(page);
 
@@ -269,26 +243,21 @@ test.describe("Transfer Service Tests", () => {
 
       await transferPage.accountForm.chooseAccount(account.accountNumber);
       await transferPage.accountForm.fillRecipientName(userName);
-      await transferPage.accountForm.fillRecipientAccount(
-        account.accountNumber,
-      );
-      await transferPage.accountForm.enterAmount("0");
+      await transferPage.accountForm.fillRecipientAccount(account.accountNumber);
+      await transferPage.accountForm.enterAmount('0');
       await transferPage.confirmCheckboxClick();
 
-      await transferPage.checkAlertAndAccept(
-        BankAlert.TRANSFER_WRONG_AMOUNT,
-        () => transferPage.clickTransferButton(),
+      await transferPage.checkAlertAndAccept(BankAlert.TRANSFER_SAME_ACCOUNT, () =>
+        transferPage.clickTransferButton(),
       );
 
       const { accounts } = await steps.getCustomerAccaunts();
-      const senderAccount = accounts.find(
-        (acc) => acc.accountNumber === account.accountNumber,
-      );
+      const senderAccount = accounts.find(acc => acc.accountNumber === account.accountNumber);
       expect(senderAccount.balance).toBe(account.balance);
     });
   });
 
-  test("User should not be able to transfer without confirm checkbox", async ({
+  test('User should not be able to transfer without confirm checkbox', async ({
     page,
     withUserSession,
     authWithToken,
@@ -296,7 +265,7 @@ test.describe("Transfer Service Tests", () => {
     const userDashboard = new UserDashboard(page);
 
     const { steps, account, userName } =
-      await test.step("Precondition: create user, authorize, create account", async () => {
+      await test.step('Precondition: create user, authorize, create account', async () => {
         const {
           steps,
           token,
@@ -312,28 +281,23 @@ test.describe("Transfer Service Tests", () => {
         return { steps, account, userName };
       });
 
-    await test.step("Transfer money without confirm checkbox", async () => {
+    await test.step('Transfer money without confirm checkbox', async () => {
       await userDashboard.clickTransferButton();
       const transferPage = new TransferPage(page);
       await transferPage.titleIsVisible();
 
       await transferPage.accountForm.chooseAccount(account.accountNumber);
       await transferPage.accountForm.fillRecipientName(userName);
-      await transferPage.accountForm.fillRecipientAccount(
-        account.accountNumber,
-      );
+      await transferPage.accountForm.fillRecipientAccount(account.accountNumber);
       await transferPage.accountForm.enterAmount(account.balance);
       expect(await transferPage.checkboxIsChecked()).toBe(false);
 
-      await transferPage.checkAlertAndAccept(
-        BankAlert.TRANSFER_FILL_ALL_FIELDS,
-        () => transferPage.clickTransferButton(),
+      await transferPage.checkAlertAndAccept(BankAlert.TRANSFER_FILL_ALL_FIELDS, () =>
+        transferPage.clickTransferButton(),
       );
 
       const { accounts } = await steps.getCustomerAccaunts();
-      const senderAccount = accounts.find(
-        (acc) => acc.accountNumber === account.accountNumber,
-      );
+      const senderAccount = accounts.find(acc => acc.accountNumber === account.accountNumber);
       expect(senderAccount.balance).toBe(account.balance);
     });
   });
